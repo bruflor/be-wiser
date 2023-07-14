6bf962cb7651760857b04dd81268b1bb79595190
Bruna <brn.flor@gmail.com>
2023-07-14 20:52:49 +0100

    configuring docker and orm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express = require('express')
+import { AppDataSource } from './data-source'
 
 const app = express()
 
 app.use(express.json())
 
-app.listen(3333, ()=>console.log("Server is running!!"))
-
-
-
-
+app.listen(3333, ()=>{
+    console.log(`server is running on port 3333`)
+    
+    AppDataSource.initialize().then(() => {
+                // here you can start to work with your database
+    }).catch((error) => console.log(error))
+    })
